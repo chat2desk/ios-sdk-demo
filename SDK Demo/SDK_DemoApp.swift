78f8b7d8c30acb70eca7bfbf697b795a2bc0e47f
Rostislav Lyashev <xiscatelx@yandex.ru>
2023-02-01 22:03:32 +0600

    Initial Commit

---
+++ SDK Demo/SDK_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDK_DemoApp.swift
+//  SDK Demo
+//
+//  Created by Ростислав Ляшев on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SDK_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
